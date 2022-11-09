f8831e37532799621a53db77306fe849fa740655
GigaOrts <ortsortskhoev95@gmail.com>
2022-11-09 16:01:44 +0300

    PlayerMovement.cs fix

--- Assets/Course Library/Scripts/PlayerMovement.cs
+++ Assets/Course Library/Scripts/PlayerMovement.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerMovement
+public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] protected float speed;
     [SerializeField] protected float turnSpeed;
@@ -10,6 +10,7 @@ public class PlayerMovement
         float horizontalInput = Input.GetAxis("Horizontal") * Time.deltaTime;
         float verticalInput = Input.GetAxis("Vertical") * Time.deltaTime;
 
-
+        transform.Translate(Vector3.forward * verticalInput * speed);
+        transform.Rotate(Vector3.up * horizontalInput * turnSpeed);
     }
 }
